4193f2a3676074f99a9cf71c4beefc9ce31c8202
Matheus Goetz <matheusgoetz@gmail.com>
2023-02-26 16:18:14 -0300

    Handling multiple requests in parallel

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,13 +1,18 @@
 
-
 const pokeApi = {}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
 
 pokeApi.getPokemons = (offset =0, limit=10) => {
     const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=&{limit}'
     return fetch(url)
         .then((response) => response.json())
         .then((jsonbody) => jsonbody.results)
-        .catch((error) => console.log(error))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequest) => Promise.all(detailRequest))
+        .then((pokemonDetails) => pokemonDetails)
 }
 
 
