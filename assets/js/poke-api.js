738178c43c9652b7128fd32db4adef70a0cec6f9
Matheus Goetz <matheusgoetz@gmail.com>
2023-03-01 14:11:49 -0300

    finalizing project

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,22 +1,19 @@
-
 const pokeApi = {}
 
-function convertPokeApiDetailToPokemon(pokeDetail){
+function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
-    
+
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
-    
+
     pokemon.types = types
     pokemon.type = type
-
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
     return pokemon
-
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
@@ -25,14 +22,14 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset =0, limit=10) => {
-    const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=&{limit}'
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
     return fetch(url)
         .then((response) => response.json())
-        .then((jsonbody) => jsonbody.results)
+        .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequest) => Promise.all(detailRequest))
-        .then((pokemonDetails) => pokemonDetails)
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
-
 
